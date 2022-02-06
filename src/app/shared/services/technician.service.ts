79d2b049d6c5d932dff1d2032d0361358b93bb4b
mBaenaCh <mateo.baena@hotmail.com>
2022-02-06 18:40:34 -0500

    Feature: technician service

---
+++ src/app/shared/services/technician.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TechnicianModel } from '../models/technician';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TechnicianService {
+
+  baseUrl: string;
+  constructor(private readonly http: HttpClient) { 
+    this.baseUrl = 'http://localhost:8080/api/technicians';
+  }
+
+  getAll(): Observable<TechnicianModel[]> {
+    return this.http.get<TechnicianModel[]>(this.baseUrl);
+  }
+
+  getById(id: string): Observable<TechnicianModel> {
+    return this.http.get<TechnicianModel>(`${this.baseUrl}/${id}`);
+  }
+}
