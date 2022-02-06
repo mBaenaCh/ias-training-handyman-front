b6d3e816018ccc5e8bfd1423809cc38826dc6bec
mBaenaCh <mateo.baena@hotmail.com>
2022-02-06 17:57:52 -0500

    Feature: defining routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'technician'},
+  {path: 'technician'},
+  {path: 'technician/:id'},
+  {path: '**', redirectTo: ''},
+];
 
 @NgModule({
   declarations: [],
