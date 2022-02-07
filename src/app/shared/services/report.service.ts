47f0190bd899c659b6f62a5dc767182fb6cec5eb
mBaenaCh <mateo.baena@hotmail.com>
2022-02-06 19:03:37 -0500

    Feature: setting up reports service

--- src/app/shared/services/report.service.ts
+++ src/app/shared/services/report.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ReportModel } from '../models/report';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,20 @@ export class ReportService {
   baseUrl: string;
   constructor(private readonly http: HttpClient) { 
     this.baseUrl = "http://localhost:8080/api/reports"
+  }
+
+  create(report: ReportModel): Observable<ReportModel>{
+    const reportBody = {
+      "technicianId": report.technicianId,
+      "serviceId": report.serviceId,
+      "initDateTime": report.initDateTime,
+      "endDateTime": report.endDateTime
+    }
+
+    return this.http.post<ReportModel>(this.baseUrl, reportBody);
+  }
+
+  getWorkedHours(id: string, week: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${id}/calculate-hours/${week}`);
   }
 }
